2ca6ae9dbab9f981fe313ce30343b19798bfdcb0
Antaeseon <ridgnfu@naver.com>
2017-10-08 20:13:43 +0900

    Finish everything

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,57 @@
 #include <string.h>
-
+#include <stdio.h>
 #include "commands.h"
+#include<unistd.h>
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+	
   // TODO: Fill it!
-  return 0;
+	int i;	
+	i=chdir(argv[1]);
+	if(i==0)
+	{
+		return 0;
+	}
+	else
+	{
+		return-1;
+	}
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  	char buf[1000];
+	getcwd(buf,1000);
+	printf("%s\n",buf);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if(strcmp(argv[0],"cd")==0&&argc==2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  	
+	if(strcmp(argv[0],"pwd")==0&&argc==1)
+	{
+		return 1;
+	}
+	
+	else
+	{
+		return 0;
+	}
+ 
 }
--- src/utils.c
+++ src/utils.c
@@ -1,39 +1,35 @@
 #include "utils.h"
 #include<stdlib.h>
 #include<string.h>
+#include<stdio.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this
-	char temp[100];
-	strcpy(temp,command);
 	int num=0;
-	argc=&num;
-	int a=0;
-	argv=(char***)malloc(sizeof(char)*100);
-	for(int i=0;i<10;i++)
+	char*token;
+	char temp[100];
+	*argc=0;
+	strcpy(temp,command);
+	*argv=(char**)malloc(sizeof(char*)*50);
+	(*argv)[num]=(char*)malloc(sizeof(char)*50);
+
+	token=strtok(temp," \n\t");
+	
+	if(token==NULL)
 	{
-		argv[i]=(char**)malloc(sizeof(char)*100);
-	}
-	for(int i=0;i<10;i++)
-	{
-		*argv[i]=(char*)malloc(sizeof(char)*100);
+		strcpy((*argv)[num],"");
+		*argc =1;
+		return;
 	}
 	
-	for(int i=0;i<=strlen(temp);i++)
+	
+	while(token!=NULL)
 	{
-		if(temp[i]!=' ')
-		{
-			(*argv[num])[a++]=temp[i];
-			if(temp[i+1]==' '||temp[i+1]==0)
-			{
-				(*argv[num])[a]=0;
-				num++;
-				a=0;
-			}
-		}
-	}
-
-		
+		strcpy((*argv)[num++],token);
+		(*argv)[num]=(char*)malloc(sizeof(char)*50);
+		token=strtok(NULL," \n\t");
+		(*argc)++;
+	}		
 }
