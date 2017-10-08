969bb96156fb6835f6ea2724c16a6e61744ab754
Antaeseon <ridgnfu@naver.com>
2017-10-08 11:30:09 +0900

    change src/utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,39 @@
 #include "utils.h"
+#include<stdlib.h>
+#include<string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+  // TODO: Fill this
+	char temp[100];
+	strcpy(temp,command);
+	int num=0;
+	argc=&num;
+	int a=0;
+	argv=(char***)malloc(sizeof(char)*100);
+	for(int i=0;i<10;i++)
+	{
+		argv[i]=(char**)malloc(sizeof(char)*100);
+	}
+	for(int i=0;i<10;i++)
+	{
+		*argv[i]=(char*)malloc(sizeof(char)*100);
+	}
+	
+	for(int i=0;i<=strlen(temp);i++)
+	{
+		if(temp[i]!=' ')
+		{
+			(*argv[num])[a++]=temp[i];
+			if(temp[i+1]==' '||temp[i+1]==0)
+			{
+				(*argv[num])[a]=0;
+				num++;
+				a=0;
+			}
+		}
+	}
+
+		
 }
